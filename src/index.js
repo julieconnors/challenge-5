f5e04902d01a7a6c96e98e31ec5ce7c89dd11ae0
Juliana Connors <65951445+julieconnors@users.noreply.github.com>
2021-06-27 05:46:20 -0400

    set state and event handler to add numbers

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import ScotchInfoBar from './ScotchInfoBar';
 import './styles.css';
 
 function App() {
+
+  const [num1, setNum] = useState(0)
+  const [num2, setNum2] = useState(0)
+
+  const [total, setTotal] = useState(0)
+
+
   return (
     <div className="App">
       <h1>Adding Two Numbers</h1>
 
       <div className="number-inputs">
-        <input type="number" placeholder="0" />
-        <input type="number" placeholder="0" />
+        <input onChange={() => setNum(num1 + 1)} type="number" placeholder="0" />
+        <input onChange={() => setNum2(num2 + 1)} type="number" placeholder="0" />
       </div>
 
-      <button>Add Them!</button>
+      <button onClick={() => setTotal(num1 + num2)}>Add Them!</button>
 
-      <h2>0</h2>
+      <h2>{total}</h2>
 
       <ScotchInfoBar seriesNumber={5} />
     </div>
